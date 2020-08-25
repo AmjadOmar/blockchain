ec5769c1866d8eba2489b8d2a311115fbd8682f2
Amjad Allobadi <aallobadi@live.com>
2020-08-25 12:48:12 -0600

    Add transactions through api

--- dev/api.js
+++ dev/api.js
@@ -23,9 +23,14 @@ app.get("/blockchain", function (req, res) {
 /* /transaction
     Allow us to create a new transaction
 */
-app.get("/transaction", function (req, res) {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
+app.post("/transaction", function (req, res) {
+  const blockIndex = bitcoin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+
+  res.json({ note: `Transaction will be added in a block ${blockIndex}.` });
 });
 
 /* /mine
